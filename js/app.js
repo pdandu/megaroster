df763e377cf742a3cbc66bdde4b9f9106bf0abc0
Pranathi <pdandu@indiana.edu>
2017-05-17 14:45:26 -0400

    Megaroster

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+
+const megaroster = { 
+    init() {
+      document
+        .querySelector('#new-student')
+        .addEventListener('submit', this.addStudent)
+
+    },
+
+    addStudent(ev){
+        ev.preventDefault()
+        const studentName = ev.target.studentName.value
+        console.log(studentName)
+    },
+}
+megaroster.init()
