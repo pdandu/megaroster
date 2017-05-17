4c7e8608a3453e9faf18ae1b93aa9d0f020fd821
Pranathi <pdandu@indiana.edu>
2017-05-17 16:05:27 -0400

    Clone template node and add remove button

--- js/app.js
+++ js/app.js
@@ -1,37 +1,46 @@
 $(document).foundation()
 
-const megaroster = { 
-    students: [],
+const megaroster = {
+  students: [],
 
-    init() {
-      this.studentList = document.querySelector('#student-list')
-      this.max = 0
-      document
-        .querySelector('#new-student')
-        .addEventListener('submit', this.addStudent.bind(this))
-    },
+  init() {
+    this.studentList = document.querySelector('#student-list')
+    this.max = 0
+    document
+      .querySelector('#new-student')
+      .addEventListener('submit', this.addStudent.bind(this))
+  },
 
-    addStudent(ev){
-        ev.preventDefault()
-        const f = ev.target
-        const student = {
-            id: this.max + 1,
-            name: f.studentName.value,
-        }
+  addStudent(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const student = {
+      id: this.max + 1,
+      name: f.studentName.value,
+    }
 
-        this.students.push(student)
-
-    const listItem = this.buildListItem(student) 
-    this.studentList.appendChild(listItem)
-
+    this.students.unshift(student)
+    
+    const listItem = this.buildListItem(student)
+    this.prependChild(this.studentList, listItem)
+    
     this.max ++
     f.reset()
-},
-    buildListItem(student) {
-        const li = document.createElement('li')
-        li.textContent = student.name
-        li.dataset.id = student.id
-        return li
-    },
-}
-megaroster.init()   
+  },
+
+  prependChild(parent, child) {
+    parent.insertBefore(child, parent.firstChild)
+  },
+
+  buildListItem(student) {
+    const template = document.querySelector('.student.template')
+    const li = template.cloneNode(true)
+    li.querySelector('.student-name').textContent = student.name
+    li.dataset.id = student.id
+    this.removeClassName(li,'template')
+    return li
+  },
+  removeClassName(el, className){
+    el.className = el.className.replace(className, '').trim()
+}}
+megaroster.init()
