2a87c551edda085aa329204cbd8b1f55e1909a24
Pranathi <pdandu@indiana.edu>
2017-05-18 09:09:00 -0400

    Students array is in sync and CSS was edited

--- js/app.js
+++ js/app.js
@@ -53,8 +53,24 @@ const megaroster = {
 
   removeStudent(ev) {
       const btn = ev.target
-      btn.closest('.student').remove()
+      const name = btn.closest('.student')
+      name.remove()
+      const id = name.getAttribute('data-id')
+      const index = this.findName(id)
+      this.students.splice(index, 1)
+      // this.students.splice(?,1)
   },
+
+  findName(index) {
+    for (let i=0; i < this.students.length; i ++){
+        if (this.students[i].id == index)
+        {
+                return i
+        }
+    }
+
+  },
+  
   removeClassName(el, className){
     el.className = el.className.replace(className, '').trim()
 }}
